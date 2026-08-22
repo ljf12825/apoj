ddbedb068c77db3f0cc2d4cab3a96a58efb8cb88
ljf12825linux <ljf12825@graingen.com>
2026-08-22 14:15:48 +0000

    update lc9.cc

--- lc9.cc
+++ lc9.cc
@@ -1,21 +1,42 @@
-// LC9 IsPalindrome
+// LC9 Palindrome Number
 
 /*
-回文数
-判断一个数是否是回文数
+Given an interget `x`, return `true` if `x` is a palindrome, and `false` otherwise
 
-边界情况
-负数、结尾是0且不等于0的数不是回文数
+Example:
+Input: x = 121
+Output: true
 */
 
-#include <iostream>
+// 存起来首尾逐个比较
+
+#include <vector>
+class Solution1 {
+public:
+    bool isPalidrome(int x) {
+        if (x < 0) return false;
+
+        std::vector<int> vec;
+        do {
+            vec.push_back(x % 10); // 空间复杂度O(n)
+            x /= 10;
+        }while (x != 0);
+
+        while (vec.size() > 1 && vec.front() == vec.back()) { // 每次循环问题规模 -2；所以外侧时间复杂度为O(n);
+            vec.pop_back();
+            vec.erase(vec.begin()); // erase是时间复杂度O(n)的操作，每次删除后一个元素后都需要把后方元素往前移动
+        } // 所以整体时间复杂度是O(n^2)
+
+        return vec.empty() || vec.size() == 1;
+    }
+};
 
 class Solution
 {
 public:
 	bool isPalindrome(int x)
 	{
-		if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+		if (x < 0 || (x % 10 == 0 && x != 0)) return false; // 负数，末尾是零的数都不是回文数，0是回文数
 
 		int reversedNumber = 0;
 
@@ -23,7 +44,7 @@ public:
 		{
 			reversedNumber = reversedNumber * 10 + x % 10;
 			x /= 10;
-		}
+		} // O(log n) n 定义为x的值
 
 		return x == reversedNumber || x == reversedNumber / 10;
 	}
