8a631cd2f1ea6b1dd1d8dc152043e17f798bc55d
ljf12825linux <ljf12825@graingen.com>
2026-08-23 14:54:10 +0000

    update lc13.cc

--- lc13.cc
+++ lc13.cc
@@ -1,116 +1,142 @@
-// title: LC13.RomanToInt
+// title: LC13.Roman to Integet
 /*
-罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+Roman numerals are repesented by seven different symbols: `I`, `V`, `X`, `L`, `C`, `D` and `M`
 
-字符          数值
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
-例如， 罗马数字 2 写做 II ，即为两个并列的 1 。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+| Symbol | Value |
+| - | - |
+| I | 1 |
+| V | 5 |
+| X | 10 |
+| L | 50 |
+| C | 100 |
+| D | 500 |
+| M | 1000 |
 
-通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，
-例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 ，数字 9 表示为 IX。
-这个特殊的规则只适用于以下六种情况：
+For example, `2` is written as `II` in Roman numeral, just two ones added together. `12` is written as `XII`, which is simply `X + II`.\
+The number `27` is written as `XXVII`, which is `XX` + `V` + `II`
 
-I 可以放在 V(5) 和 X(10) 的左边，来表示 4 和 9。
-X 可以放在 L(50) 和 C(100) 的左边，来表示 40 和 90。
-C 可以放在 D(500) 和 M(1000) 的左边，来表示 400 和 900。
-给定一个罗马数字，将其转换成整数。
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not `IIII`. Instead, the number four is written as `IV`. Because the one is before the five we substract it making four. The same principle applies to the number nine, which is written as `IX`. There are six instances where subtraction is used:
 
-示例 1:
+- `I` can be placed before `V`(5) and `X`(10) to make 4 and 9
+- `X` can be placed before `L`(50) and `C`(100) to make 40 and 90
+- `C` can be placed before `D`(500) and `M`(1000) to make 400 and 900
 
-输入: s = "III"
-输出 : 3
-示例 2 :
+Given a roman numeral, convert it to an integer
 
-输入 : s = "IV"
-输出 : 4
-示例 3 :
 
-输入 : s = "IX"
-输出 : 9
-示例 4 :
+Example 1:\
+Input: s = "III"\
+Output: 3\
+Explanation: III = 3
 
-输入 : s = "LVIII"
-输出 : 58
-解释 : L = 50, V = 5, III = 3.
-示例 5 :
+Example 2:\
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V = 5, III = 3
 
-输入 : s = "MCMXCIV"
-输出 : 1994
-解释 : M = 1000, CM = 900, XC = 90, IV = 4.
-提示：
-
-1 <= s.length <= 15
-s 仅含字符('I', 'V', 'X', 'L', 'C', 'D', 'M')
-题目数据保证 s 是一个有效的罗马数字，且表示整数在范围[1, 3999] 内
-题目所给测试用例皆符合罗马数字书写规则，不会出现跨位等情况。
-IL 和 IM 这样的例子并不符合题目要求，49 应该写作 XLIX，999 应该写作 CMXCIX 
+Example 3:\
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4
 */
 
 // 题解
 
 #include <unordered_map>
-#include <iostream>
-//不使用unordered_map
-int romanToInt(std::string s)
-{
-	auto getValue = [](char& value)->int{
-			switch (value)
-			{
-			case'I':
-				return 1;
-			case 'V':
-				return 5;
-			case 'X':
-				return 10;
-			case 'L':
-				return 50;
-			case 'C':
-				return 100;
-			case 'D':
-				return 500;
-			case 'M':
-				return 1000;
-			default:
-				return 0;
-			}
-	};
-	int sum = 0;
-	for (int i = 0; i < s.size() - 1; ++i)
-	{
-		sum += (getValue(s[i + 1]) > getValue(s[i]) ? (-getValue(s[i])) : getValue(s[i]));
-	}
-	sum += getValue(s.back());
-	return sum;
-}
 
-//使用unordered_map
-std::unordered_map<char, int> ROMAN =
-{
-	{'I', 1},
-	{'V', 5},
-	{'X', 10},
-	{'L', 50},
-	{'C', 100},
-	{'D', 500},
-	{'M', 1000},
-};
+// 使用unordered_map
 class Solution
 {
 public:
+
+    std::unordered_map<char, int> table =
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000},
+    };
+
 	int romanToInt(std::string s)
 	{
 		int ans = 0;
 		for (int i = 0; i < s.size() - 1; ++i)
 		{
-			int x = ROMAN[s[i]], y = ROMAN[s[i + 1]];
+			int x = table[s[i]], y = table[s[i + 1]]; // 核心就是判断当前位置和后面一位，从而决定当前位置的含义
 			ans += x < y ? -x : x;
 		}
-		return ans + ROMAN[s.back()];
+		return ans + table[s.back()];
 	}
 };
+
+// 使用哈希表通常涉及动态分配的节点和指针访问，访问成本相对较高
+// 使用哈希表，大致会经历
+/*
+   char
+   v
+   hash
+   v
+   bucket 定位
+   v
+   节点访问
+   v
+   比较 key
+   v
+   得到 value
+*/
+// 针对本题目，只有七个固定的场景，可以使用switch结构，编译器可将其优化成比较 + 跳转，甚至生成跳转表，条件移动等形式
+// 甚至可以使用数组
+// 甚至
+/*
+   'I' = 73
+   'V' = 86
+   'X' = 88
+   'L' = 76
+   'C' = 67
+   'D' = 68
+   'M' = 77
+*/
+// 可以使用一个很小的查找表
+// 但是数据量很小，性能差距并不大
+// 不使用unordered_map
+
+class Solution2 {
+    int romanToInt(std::string s)
+    {
+        auto getValue = [](char& value)->int{
+                switch (value)
+                {
+                case'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+                default:
+                    return 0;
+                }
+        };
+
+        int sum = 0;
+
+        for (int i = 0; i < s.size() - 1; ++i)
+        {
+            sum += (getValue(s[i + 1]) > getValue(s[i]) ? (-getValue(s[i])) : getValue(s[i]));
+        }
+
+        sum += getValue(s.back());
+        return sum;
+    }
+};
+
