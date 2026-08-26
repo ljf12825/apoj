8ca41ebef4dfd218cc74f428aa5ce400d281b4f1
ljf12825linux <ljf12825@graingen.com>
2026-08-26 15:34:57 +0000

    update lc21

--- lc21.cc
+++ lc21.cc
@@ -1,7 +1,23 @@
-// LC21.MergeTwoLinkedList
+// LC21.Merge Two Sorted Lists
 
 /*
-给定两个升序数组，合并两个数组为一个升序数组
+   You are given the heads of two sorted linked lists `list1` and `list2`
+
+   Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists
+
+   Return the head of the merged linked list.
+
+   Example 1:
+   Input: list1 = [1, 2, 4], list2 = [1, 3, 4]
+   Output: [1, 1, 2, 3, 4, 4]
+
+   Example 2:
+   Input: list1 = [], list2 = []
+   Output: []
+
+   Example3:
+   Input: list1 = [], list2 = [0]
+   Output: [0]
 */
 
 struct LinkedNode
@@ -16,46 +32,45 @@ struct LinkedNode
 class Solution_Recursion
 {
 public:
-	LinkedNode* mergeTwo(LinkedNode* list1, LinkedNode* list2)
+	LinkedNode* mergeTwoLists(LinkedNode* list1, LinkedNode* list2)
 	{
 		if (list1 == nullptr) return list2;
 		else if (list2 == nullptr) return list1;
 		else if (list1->val < list2->val)
 		{
-			list1->next = mergeTwo(list1->next, list2);
+			list1->next = mergeTwoLists(list1->next, list2);
 			return list1;
 		}
 		else
 		{
-			list2->next = mergeTwo(list1, list2->next);
+			list2->next = mergeTwoLists(list1, list2->next);
 			return list2;
 		}
 	}
 };
 
-class Solution_Iteration
+class Solution_Iteration // 这是我一开始想到的写法，这道题思路很简单，但是我被指针搞的很烦
 {
 public:
-	LinkedNode* mergeTwo(LinkedNode* list1, LinkedNode* list2)
+	LinkedNode* mergeTwoLists(LinkedNode* list1, LinkedNode* list2)
 	{
-		LinkedNode* templist = new LinkedNode();
-		LinkedNode* head = templist;
+		LinkedNode faker; // 这是一个哨兵节点，用于指引整个list
+		LinkedNode* list3 = &faker; // 这是提供操作的指针
 		while (list1 && list2)
 		{
 			if (list1->val < list2->val)
 			{
-				templist->next = list1;
+				list3->next = list1;
 				list1 = list1->next;
 			}
 			else
 			{
-				templist->next = list2;
+				list3->next = list2;
 				list2 = list2->next;
 			}
-			templist = templist->next;
+			list3 = list3->next;
 		}
-		templist->next = list1 == nullptr ? list2 : list1;
-		delete templist;
-		return head->next;
+		list3->next = list1 == nullptr ? list2 : list1;
+		return faker.next;
 	}
 };
