f60bdb1f2c0ff143389a46228e1d489db35a4195
ljf12825linux <ljf12825@graingen.com>
2026-09-01 15:21:15 +0000

    update lc66

--- lc66.cc
+++ lc66.cc
@@ -1,27 +1,42 @@
-// LC66.AddOne
+// LC66. Plus One
 
 /*
-用一个vector<int>表示一个十进制非负整数
-输入vector<int>
-输出末位+1后的新数组
+   You are given a large integer represented as an integer array `digits`, where each `digits[i]` is the `ith` digit of the intteger.\
+   The digits are ordered from most significant to least significant in left-to-right order. The large integer dose not contain any leading `0`'s.
+
+   Increment the large integer by on and return the resulting array of digits.
+
+   Example1:
+   Input: digits = [1, 2, 3]
+   Output: [1, 2, 4]
+   Explanation: The array represents the integer 123.\
+   Incrementing by one give 123 + 1 = 124.\
+   Thus, the result should be [1, 2, 4]
+
+   Example2:
+   Input: digits = [9]
+   Output: [1, 0]
+   Explanation: The array represents the integer 9.\
+   Incrementing by one gives 9 + 1 = 10.\
+   Thus, the result should be [1, 0]
 */
 
 #include <vector>
 class Solution
 {
 public:
-	std::vector<int>addOne(std::vector<int>& digits)
+	std::vector<int>plusOne(std::vector<int>& digits)
 	{
 		for (auto it = digits.rbegin(); it != digits.rend(); ++it)
 		{
-			if (*it < 9)
+			if (*it < 9) // 只要遇到不是9的数，该位置加1就能结束问题
 			{
 				(*it)++;
 				return digits;
 			}
 			*it = 0;
 		}
-		digits.insert(digits.begin(), 1);
+		digits.insert(digits.begin(), 1); // 循环完毕说明首位是0，首位insert 1
 		return digits;
 	}
 };
