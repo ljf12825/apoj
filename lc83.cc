a8fc7475931cf9a33c949d994efb4f508fc8aa0f
ljf12825linux <ljf12825@graingen.com>
2026-09-05 15:10:50 +0000

    init commit lc83

---
+++ lc83.cc
@@ -0,0 +1,36 @@
+// LC83. Remove Duplicates from Sorted List
+
+/*
+   Given the `head` of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well
+
+   Example 1:
+   Input: head = [1,1,2]
+   Output: [1,2]
+
+   Example 2:
+   Input: head = [1,1,2,3,3]
+   Output: [1,2,3]
+*/
+
+// Definition for singly linked list
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* cur = head;
+        if (head == nullptr) return head;
+        while (head == nullptr) {
+            if (cur->next->val == cur->val) cur->next = cur->next->next;
+            else cur = cur->next;
+        }
+
+        return head;
+    }
+};
