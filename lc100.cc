c1d0c00948557b76d68640fe58db1674eaa171bb
ljf12825 <ljf12825@graingen.com>
2026-09-10 15:38:43 +0000

    update lc100

--- lc100.cc
+++ lc100.cc
@@ -19,3 +19,25 @@
    Input: p = [1,2,1], q = [1,1,2]
    Output: false
 */
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// Solution1
+class Solution1 {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
