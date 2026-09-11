0e4c17fe12fdb19319106abd58a779ea7ca7f311
ljf12825 <ljf12825@graingen.com>
2026-09-11 15:13:15 +0000

    update lc100

--- lc100.cc
+++ lc100.cc
@@ -21,6 +21,10 @@
 */
 
 // Definition for a binary tree node.
+#include <queue>
+#include <vector>
+#include <climits>
+
 struct TreeNode {
     int val;
     TreeNode* left;
@@ -30,7 +34,7 @@ struct TreeNode {
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
-// Solution1
+// Solution1 递归 DFS
 class Solution1 {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
@@ -39,5 +43,55 @@ public:
         if (p->val != q->val) return false;
 
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+// Solution2 用queue BFS
+class Solution2 {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        std::queue<TreeNode*> que;
+        que.push(p);
+        que.push(q);
+
+        while (!que.empty()) {
+            TreeNode* node1 = que.front();
+            que.pop();
+            TreeNode* node2 = que.front();
+            que.pop();
+
+            if (node1 == nullptr && node2 == nullptr) continue;
+            if (node2 == nullptr || node2 == nullptr) return false;
+            if (node1->val != node2->val) return false;
+
+            que.push(node1->left);
+            que.push(node2->left);
+            que.push(node1->right);
+            que.push(node2->right);
+        }
+
+        return true;
+    }
+};
+
+// Solution3 前序/中序/后序遍历 + 可视化空节点
+class Solution3 {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        std::vector<int> vec1, vec2;
+        preorder(p, vec1);
+        preorder(q, vec2);
+        return vec1 == vec2;
+    }
+
+    void preorder(TreeNode* root, std::vector<int>& result) {
+        if (root == nullptr) {
+            result.push_back(INT_MIN); // 这样写的前提需要保证用例里没有INT_MIN，否则不成立
+            return;
+        }
+
+        result.push_back(root->val);
+        preorder(root->left, result);
+        preorder(root->right, result);
     }
 };
