2671eff08faa52376ffc735ba477f61fbdd98317
ljf12825 <ljf12825@graingen.com>
2026-09-16 15:58:55 +0000

    init commit lc3

---
+++ lc3.cc
@@ -0,0 +1,20 @@
+// LC3. Longest Substring Without Repeating Characters
+
+/*
+   Given a string `s`, find the length of the longest substring without duplicate characters.
+*/
+
+/*
+   Example1:
+   Input: s = "abcabcbb"
+   Output: 3
+   Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+
+   Example2:
+   Input: s = "bbbbb"
+   Output: 1
+
+   Example3:
+   Input: s = "pwwkew"
+   Output: 3
+*/
