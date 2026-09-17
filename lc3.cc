7303b17f50e16b94491a4fc57ca888ce430a1788
ljf12825 <ljf12825@graingen.com>
2026-09-17 15:17:20 +0000

    update lc3

--- lc3.cc
+++ lc3.cc
@@ -19,21 +19,50 @@
    Output: 3
 */
 
+#include <algorithm>
+#include <string>
+#include <vector>
+
 // Solution1 暴力解法 时间复杂度O(n^2 * m)
 class Solution1 {
 public:
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring(std::string s) {
         int length = 0;
         for (int i = 0; i < s.length(); ++i) {
-            string ss;
+            std::string ss;
             for (int j = i; j < s.length(); ++j) {
-                if (ss.find(s[j]) == string::npos) {
+                if (ss.find(s[j]) == std::string::npos) {
                     ss.push_back(s[j]);
-                } else break;
+                } else break; // 直接跳出循环，清空ss的内容
             }
             length = length > ss.length() ? length : ss.length();
         }
 
         return length;
+    }
+};
+
+// Solution2 滑动窗口：优化暴力解法，不清除窗口改为往后移动，这样可以减少重复动作
+class Solution2 {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        int ans = 0;
+        int left = 0;
+
+        std::vector<int> last(256, -1);
+
+        for (int right = 0; right < s.size(); ++right) {
+            char c = s[right];
+
+            if (last[c] >= left) { // 隐式类型转换，如果字符串只包含ASCII字符(0~127)，安全；
+                left = last[c] + 1; // 一旦发现last[c]的值不是-1，说明之前有这个字符，那就把left移动到这个字符后面
+            }
+
+            last[c] = right; // 记录每个char的位置
+
+            ans = std::max(ans, right - left + 1);
+        }
+
+        return ans;
     }
 };
