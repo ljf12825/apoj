fe3c2e71a4c195ad0399e487d0fb855c9c88c017
ljf12825 <ljf12825@graingen.com>
2026-09-18 15:25:43 +0000

    init commit lc110

---
+++ lc110.cc
@@ -0,0 +1,19 @@
+// LC110. Balanced Binary Tree
+
+/*
+   Given a binary tree, determine if it is height-balanced(A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.)
+*/
+
+/*
+   Example1:
+   Input: root = [3,9,20,null,null,15,7]
+   Output: true
+
+   Example2:
+   Input: root = [1,2,2,3,3,null,null,4,4]
+   Output: false
+
+   Example3:
+   Input: root = []
+   Output: true
+*/
