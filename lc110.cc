90e04c5c15db4af3784159ac72928647511ed53b
ljf12825 <ljf12825@graingen.com>
2026-09-18 15:46:11 +0000

    update lc110

--- lc110.cc
+++ lc110.cc
@@ -17,3 +17,30 @@
    Input: root = []
    Output: true
 */
+
+// Definition for a binary tree node
+#include <algorithm>
+#include <cstdlib>
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// Solution1 Top-Down DFS，写一个判断树高的辅助函数，递归对每个节点判断它的左右子树是否平衡; 时间复杂度最坏O(n^2)，空间复杂度O(n)
+class Solution1 {
+public:
+    bool isBalanced (TreeNode* root) {
+        if (root == nullptr) return true;
+        bool isCurrentBalanced = abs(treeheight(root->left) - treeheight(root->right)) <= 1;
+        return isCurrentBalanced && isBalanced(root->left) && isBalanced(root->right);
+    }
+
+    int treeheight(TreeNode* node) {
+        if (node == nullptr) return 0;
+        return 1 + std::max(treeheight(node->left), treeheight(node->right));
+    }
+};
