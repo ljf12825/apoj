47774951183418eed5b3521c8867a4b5078a108b
ljf12825 <ljf12825@graingen.com>
2026-09-21 15:47:21 +0000

    init commit lc111

---
+++ lc111.cc
@@ -0,0 +1,16 @@
+// LC111. Minimum Depth of Binary Tree
+
+/*
+   Given a binary tree, find its minimum depth.\
+   The minimum is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+*/
+
+/*
+   Example1:
+   Input: root = [3,9,20,null,null,15,7]
+   Output: 2
+
+   Example2:
+   Input: root = [2,null,3,null,4,null,5,null,6]
+   Output: 5
+*/
