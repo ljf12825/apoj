9229eb7174c820b53a71dccc45f0fea374165723
ljf12825 <ljf12825@graingen.com>
2026-09-23 15:40:50 +0000

    init commit lc5

---
+++ lc5.cc
@@ -0,0 +1,15 @@
+// LC5. Longest Palindromic Substring
+
+/*
+   Given a string `s`, return the longest palindromic substring in `s`
+*/
+
+/*
+   Example1:
+   Input: s = "babab"
+   Output: "bab"
+
+   Example2:
+   Input: s = "cbbd"
+   Output: "bb"
+*/
