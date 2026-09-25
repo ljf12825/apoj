df66bcc353276151cfe5b3eb8e08494dc41f21f5
ljf12825 <ljf12825@graingen.com>
2026-09-25 15:23:07 +0000

    init commit lc112

---
+++ lc112.cc
@@ -0,0 +1,22 @@
+// LC112. Path Sum
+
+/*
+   Given the `root` of a binary tree and an integer `targetSum`, return `true` if the tree has a root-to-leaf path such that adding up all the values along the path equals `targetSum`.
+
+   A leaf is a node with no children.
+*/
+
+/*
+   Example1:\
+   Input: root = [5,4,8,11,null,13,4,,7,2,null,null,null,1], targetSum = 22\
+   Output: true\
+   Explanation: 5->4->11->2
+
+   Example2:\
+   Input: root = [1,2,3], targetSum = 5
+   Output: false
+
+   Example3:\
+   Input: root = [], targetSum = 0
+   Output: false
+*/
