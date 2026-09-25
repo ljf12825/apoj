cffd2ef842085ff635001e10146090c611c5efe2
ljf12825 <ljf12825@graingen.com>
2026-09-25 15:48:39 +0000

    update lc112

--- lc112.cc
+++ lc112.cc
@@ -20,3 +20,86 @@
    Input: root = [], targetSum = 0
    Output: false
 */
+
+// Definition for a binary tree node.
+
+#include <stack>
+#include <utility>
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// MySolution 这是错误的
+class MySolution1 {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        // 问题1：当Input为root = [], targetSum == 0时返回true，应该返回false
+        // 问题2：返回true的判定条件是叶子节点且路径和为targetSum，现在是以targetSum == 0作为前提，推到一个空节点上导致无法判断上一个节点是否为叶子节点
+        if (!root && targetSum == 0) return true;
+        else if (!root && targetSum != 0) return false;
+        else if (root && targetSum == 0) return false;
+        else return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+};
+
+class MySolution2 {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (flag == 0 && root == nullptr) return false; // 用flag为0的情况判断树根为nullptr的情况，比较丑陋
+        if (!root && targetSum == 0) { // 但是问题2仍然存在
+            flag++;
+            return true;
+        } else if (!root && targetSum != 0) {
+            flag++;
+            return false;
+        } else if (root && targetSum == 0) {
+            flag++;
+            return false;
+        } else {
+            flag++;
+            return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+        }
+
+    }
+
+private:
+    int flag = 0;
+};
+
+// Solution1：递归，在叶子节点处判断targetSum的情况，一举两得
+class Solution1 {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+        if (root->left == nullptr && root->right == nullptr) return targetSum == root->val;
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+};
+
+// Solution2：迭代，DFS + 栈，手动保存状态
+class Solution2 {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+
+        std::stack<std::pair<TreeNode*, int>> sta;
+        sta.push({root, targetSum});
+        
+        while (!sta.empty()) {
+            auto [node, sum] = sta.top();
+            sta.pop();
+
+            if (node->left == nullptr && node->right == nullptr) {
+                if (sum == node->val) return true;
+            }
+            if (node->right) sta.push({node->right, sum - node->val});
+            if (node->left) sta.push({node->left, sum - node->val});
+        }
+        return false;
+    }
+};
