48714fb0ed6bed77ab84da457c6bcbaf406dfb99
ljf12825 <ljf12825@graingen.com>
2026-09-26 07:39:30 +0000

    init commit lc6

---
+++ lc6.cc
@@ -0,0 +1,34 @@
+// LC6. Zigzag Conversion
+
+/*
+   The string `"PAYPALISHIRING"` is written in a zigzag pattern on a given numvver of rows like this:(you may want to display this pattern in a fixed font for better legibility)
+   ```txt
+   P A H N
+   APLSIIG
+   Y I R
+   ```
+
+   And then read line by line: `"PAHNAPLSIIGYIR"`
+
+   Write the code that will take a string and make this conversion given a number of rows:
+
+   `string convert(string s, int numRows);`
+*/
+
+/*
+   Example1:\
+   Input: s = "PAYPALISHIRING", numRows = 3\
+   Output: "PAHNAPLSIIGYIR"
+
+   Example2:\
+   Input: s = "PAYPALISHIRING", numRows = 4\
+   Output: "PINALSIGYAHRPI"\
+   Explanation:
+
+   ```txt
+   P  I  N
+   A LS IG
+   YA HR
+   P  I
+   ```
+*/
